15a25ac9453b37fdf7ef852c5e8ff1e2afb51d15
Türkay Biliyor <turkay.biliyor@iys.org.tr>
2025-01-22 22:12:54 +0300

    fix

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -349,6 +349,7 @@ void MainWindow::processPressureData(const QList<qreal>& info)
     if (lastPressTimestamp > 0) {
         updatePressureAndAltitude();
     }
+
     lastPressTimestamp = timestamp;
 }
 
